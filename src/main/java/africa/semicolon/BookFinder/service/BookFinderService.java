1f4c6a40feca93f9dedc3ae294b4b833608f0b48
Adeshina Qudus <qudusa55@gmail.com>
2024-03-27 17:11:53 +0100

    took out anythin related to book finder

--- src/main/java/africa/semicolon/BookFinder/service/BookFinderService.java
+++
@@ -1,8 +0,0 @@
-package africa.semicolon.BookFinder.service;
-
-import africa.semicolon.BookFinder.dtos.request.BookFinderRequest;
-import africa.semicolon.BookFinder.dtos.response.BookFinderResponse;
-
-public interface BookFinderService {
-    BookFinderResponse searchBook(BookFinderRequest request);
-}
